469f8d011ce14a299de348bd06431664d3eb61b3
Natsu <104824717+nnnnatsu@users.noreply.github.com>
2024-07-01 19:26:04 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-chmod +x setup.sh
-python install_packages.py
+# Ensure distutils is installed
+pip install setuptools
+pip install wheel
+pip install distutils
+
+# Proceed with the rest of the installations
+pip install -r requirements.txt
+
 PORT=${PORT:-8501}
 export PORT
 streamlit run streamlit_app.py --server.port $PORT
